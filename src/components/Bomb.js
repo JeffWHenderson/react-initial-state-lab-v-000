22728b40fb1dbe979dd3a28419f6934bb435ae4e
jeffrey henderson <jeffrey.henderson17@gmail.com>
2018-01-28 00:48:08 +0000

    automatically backed up by learn

--- src/components/Bomb.js
+++ src/components/Bomb.js
@@ -1,6 +1,6 @@
 // Bomb Component Code Goes Here
-import 'React' from react
-import 'ReactDom' from "react-dom"
+import React from "react"
+import ReactDom from "react-dom"
 
 class Bomb extends React.Component {
   render(){
