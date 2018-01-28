b5c9421186ae1987995e1ccdf0cfebf4dc24bcfa
jeffrey henderson <jeffrey.henderson17@gmail.com>
2018-01-28 01:13:49 +0000

    automatically backed up by learn

--- src/components/Bomb.js
+++ src/components/Bomb.js
@@ -12,7 +12,7 @@ class Bomb extends React.Component {
 
 
   render(){
-    let time = this.state.secondsLeft === 0 ? "Boom!" : `${this.state.secondsLeft}`
+    let time = this.state.secondsLeft === 0 ? "Boom!" : `${this.state.secondsLeft} seconds left before I go boom!`
     return (
       <div>
         <h1>{time}</h1>
