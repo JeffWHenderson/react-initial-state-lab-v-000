dc0e5c87bf54d8d385d7bff05688d0f73585b2c4
jeffrey henderson <jeffrey.henderson17@gmail.com>
2018-01-28 01:01:42 +0000

    automatically backed up by learn

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -4,9 +4,16 @@ import ReactDom from 'react-dom'
 class ImageSlider extends React.Component {
   constructor() {
     super()
-    
-    <div><h1>I am on slide CURRENT_SLIDE</h1></div>
-    
+
+    this.state = {
+      isEnabled: false
+    }
+  }
+
+  render() {
+    return(
+        <div><h1>I am on slide CURRENT_SLIDE</h1></div>
+    )
   }
 }
 
