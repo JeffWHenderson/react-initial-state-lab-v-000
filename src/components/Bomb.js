eb76f1f33460fa8ea8fbff17c1cc7ee4b293cfd3
jeffrey henderson <jeffrey.henderson17@gmail.com>
2018-01-28 00:46:26 +0000

    automatically backed up by learn

--- src/components/Bomb.js
+++ src/components/Bomb.js
@@ -1,1 +1,15 @@
 // Bomb Component Code Goes Here
+import 'React' from react
+import 'ReactDom' from "react-dom"
+
+class Bomb extends React.Component {
+  render(){
+    return (
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    )
+  }
+}
+
+export default Bomb 
